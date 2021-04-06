bd9101f410b71de7a0eb28dfc1ad8a6da5c441be
jappelgren <justinappelgren@gmail.com>
2021-04-06 17:41:55 -0500

    Random message, random image will be posted to tweet. when image is posted it will be removed from images table. if no images in table no tweet is made.

--- server.js
+++ server.js
@@ -24,32 +24,34 @@ const T = new Twit({
 
 const imageTest = fs.readFileSync('images/gorilly.jpg', { encoding: 'base64' });
 
-cron.schedule(' 1-59 * * * * *', async () => {
+cron.schedule(' 0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', async () => {
   const image = await pool.query('SELECT * FROM images;');
   const words = await pool.query('SELECT * FROM words;');
 
   const randomImageIndex = Math.floor(Math.random() * (image.rows.length - 0) + 0)
   const randomWordsIndex = Math.floor(Math.random() * (words.rows.length - 0) + 0)
 
-  await console.log(image.rows[randomImageIndex], words.rows[randomWordsIndex]);
+  if(image.rows.length > 0) {
+    await T.post('media/upload', { media_data: fs.readFileSync(`images/${image.rows[randomImageIndex].image}`, { encoding: 'base64' }) }, async (err, data, response) => {
 
-  // T.post('media/upload', { media_data: imageTest }, function (err, data, response) {
-
-  //     const mediaIdStr = data.media_id_string
-  //     const altText = "A powerful gorilly making a gd face."
-  //     const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
-
-  //     T.post('media/metadata/create', meta_params, function (err, data, response) {
-  //       if (!err) {
-
-  //         const params = { status: 'Wow, what a gorilly.', media_ids: [mediaIdStr] }
-
-  //         T.post('statuses/update', params, function (err, data, response) {
-  //           console.log(data)
-  //         })
-  //       }
-  //     })
-  //   })
+        const mediaIdStr = data.media_id_string
+        const altText = "An item which if you viewed it you would look at someone and say if you know, you know, or this item makes whatever I am experiencing right now \"hit different\""
+        const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+  
+       await T.post('media/metadata/create', meta_params, async(err, data, response) => {
+          if (!err) {
+  
+            const params = { status: `${words.rows[randomWordsIndex].tweet_text} 🍧`, media_ids: [mediaIdStr] }
+  
+            T.post('statuses/update', params, async (err, data, response) => {
+             await pool.query(`DELETE FROM images WHERE id = ${image.rows[randomImageIndex].id}`)
+            })
+          }
+        })
+      })
+  } else {
+      console.log('No images in database')
+  }
   
 });
 
