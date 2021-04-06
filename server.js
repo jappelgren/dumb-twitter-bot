4ecd7bc0c26ccef60ff9843453c0abf3dcdba761
jappelgren <justinappelgren@gmail.com>
2021-04-06 15:48:13 -0500

    Added functionality to post images with words

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = Express();
 const PORT = 5000;
 const Twit = require('twit');
 const cron = require('node-cron');
+const fs = require('fs')
 require('dotenv').config();
 
 const consumer_key = process.env.API_KEY;
@@ -19,17 +20,29 @@ const T = new Twit({
   strictSSL: true,
 });
 
-T.post(
-  'statuses/update',
-  { status: 'hello world!' },
-  function (err, data, response) {
-    console.log(data);
-  }
-);
+const imageTest = fs.readFileSync('images/gorilly.jpg', { encoding: 'base64' })
 
-// cron.schedule(' 0-59 * * * * *', () => {
-//   console.log('hi');
-// });
+
+cron.schedule(' 0 * * * * *', () => {
+    T.post('media/upload', { media_data: imageTest }, function (err, data, response) {
+  
+        const mediaIdStr = data.media_id_string
+        const altText = "A powerful gorilly making a gd face."
+        const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+       
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+          if (!err) {
+            
+            const params = { status: 'Wow, what a gorilly.', media_ids: [mediaIdStr] }
+       
+            T.post('statuses/update', params, function (err, data, response) {
+              console.log(data)
+            })
+          }
+        })
+      })
+      console.log('metro boomin want some more.')
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
