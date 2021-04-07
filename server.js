b13441b71a8334afb91e22d34c4309921b5e5523
jappelgren <justinappelgren@gmail.com>
2021-04-07 12:19:39 -0500

    trouble shooting app not working on raspberry pi

--- server.js
+++ server.js
@@ -22,37 +22,41 @@ const T = new Twit({
   timeout_ms: 60 * 1000,
   strictSSL: true,
 });
-
-cron.schedule(' 0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', async () => {
-  const image = await pool.query('SELECT * FROM images;');
-  const words = await pool.query('SELECT * FROM words;');
-
-  const randomImageIndex = Math.floor(Math.random() * (image.rows.length - 0) + 0)
-  const randomWordsIndex = Math.floor(Math.random() * (words.rows.length - 0) + 0)
-
-  if(image.rows.length > 0) {
-    await T.post('media/upload', { media_data: fs.readFileSync(`images/${image.rows[randomImageIndex].image}`, { encoding: 'base64' }) }, async (err, data, response) => {
-
-        const mediaIdStr = data.media_id_string
-        const altText = "An item which if you viewed it you would look at someone and say \"if you know, you know\", or this item makes whatever I am experiencing right now \"hit different\""
-        const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+try {
+  cron.schedule(' 0-59 * * * * *', async () => {
+    const image = await pool.query('SELECT * FROM images;');
+    const words = await pool.query('SELECT * FROM words;');
   
-       await T.post('media/metadata/create', meta_params, async(err, data, response) => {
-          if (!err) {
+    const randomImageIndex = Math.floor(Math.random() * (image.rows.length - 0) + 0)
+    const randomWordsIndex = Math.floor(Math.random() * (words.rows.length - 0) + 0)
   
-            const params = { status: `${words.rows[randomWordsIndex].tweet_text} 🍧`, media_ids: [mediaIdStr] }
+    if(image.rows.length > 0) {
+      await T.post('media/upload', { media_data: fs.readFileSync(`images/${image.rows[randomImageIndex].image}`, { encoding: 'base64' }) }, async (err, data, response) => {
   
-            T.post('statuses/update', params, async (err, data, response) => {
-             await pool.query(`DELETE FROM images WHERE id = ${image.rows[randomImageIndex].id};`)
-            })
-          }
+          const mediaIdStr = data.media_id_string
+          const altText = "An item which if you viewed it you would look at someone and say \"if you know, you know\", or this item makes whatever I am experiencing right now \"hit different\""
+          const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+    
+         await T.post('media/metadata/create', meta_params, async(err, data, response) => {
+            if (!err) {
+    
+              const params = { status: `${words.rows[randomWordsIndex].tweet_text} 🍧`, media_ids: [mediaIdStr] }
+    
+              T.post('statuses/update', params, async (err, data, response) => {
+               await pool.query(`DELETE FROM images WHERE id = ${image.rows[randomImageIndex].id};`)
+              })
+            }
+          })
         })
-      })
-  } else {
-      console.log('No images in database')
-  }
+    } else {
+        console.log('No images in database')
+    }
+    
+  });
   
-});
+} catch (error) {
+  console.log(error)
+}
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
