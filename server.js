1580d9c952b60b508592fb61426e2195e963efff
jappelgren <justinappelgren@gmail.com>
2021-04-10 00:26:44 -0500

    fixed nodecron to run function every day at noon

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ async function postTweet() {
   }
 }
 
-cron.schedule(' 0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', async () => {
+cron.schedule('0 0 12 * * *', async () => {
   postTweet()
 });
 
