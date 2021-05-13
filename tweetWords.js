e3b44bec2964981ecaefc3d32a9d5455fe4f6a26
Justin <justinappelgren@gmail.com>
2021-05-13 16:31:55 +0100

    Added hastages to tweet words

--- tweetWords.js
+++ tweetWords.js
@@ -1,7 +1,7 @@
 const words = [
-    'If you know, you know',
-    'Things just hit different when you have one of these around.',
-    'Only real ones will feel this.'
+    'If you know, you know #ifyouknowyouknow #hitdifferent realones',
+    'Things just hit different when you have one of these around. #ifyouknowyouknow #hitdifferent realones',
+    'Only real ones will feel this. #ifyouknowyouknow #hitdifferent realones'
 ]
 
 module.exports = words
