8768bb9467608d9334ba6ce2aeb23223f9b4fb2e
Justin Appelgren <justinappelgren@gmail.com>
2021-05-14 19:58:59 +0100

    fixed typo in tweet words

--- tweetWords.js
+++ tweetWords.js
@@ -1,7 +1,7 @@
 const words = [
-    'If you know, you know #ifyouknowyouknow #hitdifferent realones',
-    'Things just hit different when you have one of these around. #ifyouknowyouknow #hitdifferent realones',
-    'Only real ones will feel this. #ifyouknowyouknow #hitdifferent realones'
+    'If you know, you know #ifyouknowyouknow #hitdifferent #realones',
+    'Things just hit different when you have one of these around. #ifyouknowyouknow #hitdifferent #realones',
+    'Only real ones will feel this. #ifyouknowyouknow #hitdifferent #realones'
 ]
 
 module.exports = words
