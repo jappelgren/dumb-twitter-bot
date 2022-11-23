712f2d6c0be45b785afb2238e50c747b1e551121
jappelgren <justinappelgren@gmail.com>
2022-11-23 09:36:49 -0600

    refined search terms, added new tweet message

--- variables/tweetWords.js
+++ variables/tweetWords.js
@@ -2,4 +2,5 @@ export const words = [
   "If you know, you know.",
   "Things just hit different when you have one of these around.",
   "Only real ones will feel this.",
+  "Who remembers these? They went crazy."
 ];
